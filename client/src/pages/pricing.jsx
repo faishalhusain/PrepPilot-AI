9d43bda42723798fd64c9390b6d49649eecd4f7d
Faishal <faishalh994@gmail.com>
2026-04-01 01:16:43 +0530

    Completed

--- client/src/pages/pricing.jsx
+++ client/src/pages/pricing.jsx
@@ -2,10 +2,16 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { FaArrowLeft, FaCheckCircle } from 'react-icons/fa';
 import { motion } from "motion/react"
+import { serverUrl } from '../App';
+import axios from "axios"
+import { useDispatch } from 'react-redux';
+import { setUserData } from '../redux/userSlice';
 
 function Pricing() {
   const navigate = useNavigate()
   const [selectedPlan, setSelectedPlan] = useState("free")
+  const [loadingPlan, setLoadingPlan] = useState(null)
+  const dispatch = useDispatch()
 
   const plans = [
     {
@@ -50,6 +56,60 @@ function Pricing() {
       badge: "Best Value",
     },
   ]
+
+  const handlePayment = async (plan) => {
+    try {
+      setLoadingPlan(plan.id)
+
+      const amount =
+        plan.id === "basic" ? 100 :
+          plan.id === "pro" ? 500 : 0;
+
+      const result = await axios.post(serverUrl + "/api/payment/order", {
+        planId: plan.id,
+        amount: amount,
+        credits: plan.credits,
+      }, { withCredentials: true })
+      console.log(result.data)
+
+      const options = {
+        key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+        amount: result.data.amount,
+        currency: "INR",
+        name: "PrepPilot-AI",
+        description: `${plan.name} - ${plan.credits} Credits`,
+        order_id: result.data.id,
+
+        handler: async function (response) {
+          try {
+            const verifypay = await axios.post(
+              serverUrl + "/api/payment/verify",
+              response,
+              { withCredentials: true }
+            )
+            dispatch(setUserData(verifypay.data.user))
+
+            alert("Payment Successful 🎉 Credits Added!");
+            navigate("/")
+          } catch (error) {
+            console.log(error)
+            alert("Payment verification failed ❌")
+          }
+        },
+        theme: {
+          color: "#10b981",
+        },
+      }
+      const rzp = new window.Razorpay(options)
+      rzp.open()
+
+      setLoadingPlan(null);
+
+    } catch (error) {
+      console.log(error)
+      setLoadingPlan(null);
+    }
+  }
 
   return (
     <div className='min-h-screen bg-gradient-to-br from-gray-50 to-emerald-5 py-16 px-6'>
@@ -115,12 +175,12 @@ function Pricing() {
                   {plan.credits} Credits
                 </p>
               </div>
-{/* Description */}
+              {/* Description */}
               <p className="text-gray-500 mt-4 text-sm leading-relaxed">
                 {plan.description}
               </p>
 
-{/* Features */}
+              {/* Features */}
               <div className="mt-6 space-y-3 text-left">
                 {plan.features.map((feature, i) => (
                   <div key={i} className="flex items-center gap-3">
@@ -133,12 +193,25 @@ function Pricing() {
               </div>
               {!plan.default && (
                 <button
+                  disabled={loadingPlan === plan.id}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    if (!isSelected) {
+                      setSelectedPlan(plan.id)
+                    } else {
+                      handlePayment(plan)
+                    }
+                  }}
                   className={`w-full mt-8 py-3 rounded-xl font-semibold transition ${isSelected
-                      ? "bg-emerald-600 text-white hover:opacity-90"
-                      : "bg-gray-100 text-gray-700 hover:bg-emerald-50"
+                    ? "bg-emerald-600 text-white hover:opacity-90"
+                    : "bg-gray-100 text-gray-700 hover:bg-emerald-50"
                     }`}
                 >
-                  {isSelected ? "Proceed to Pay" : "Select Plan"}
+                  {loadingPlan === plan.id
+                    ? "Processing..."
+                    : isSelected
+                      ? "Proceed to Pay"
+                      : "Select Plan"}
                 </button>
               )}
 
--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -1,44 +1,45 @@
-import payment from "../models/payment.model";
-import razorpay from "../services/razorpay.service";
+import payment from "../models/payment.model.js";
+import razorpay from "../services/razorpay.service.js";
 import crypto from "crypto"
+import User from "../models/user.model.js";
 
 export const createOrder = async (req, res) => {
-    try {
-        const { planId, amount, credits } = req.body;
-        if (!amount || !credits) {
-            return res.status(400).json({ message: "Invalid plan data" });
-        }
-
-        const options = {
-            amount: amount * 100, // convert to paise
-            currency: "INR",
-            receipt: `receipt_${Date.now()}`,
-        };
-
-        const order = await razorpay.orders.create(options)
-
-        await payment.create({
-            userId: req.userId,
-            planId,
-            amount,
-            credits,
-            razorpayOrderId: order.id,
-            status: "created",
-        });
-
-        return res.json(order);
-
-    } catch (error) {
-        return res.status(500).json({message:`failed to get razorpay order ${error}`})
+  try {
+    const { planId, amount, credits } = req.body;
+    if (!amount || !credits) {
+      return res.status(400).json({ message: "Invalid plan data" });
     }
+
+    const options = {
+      amount: amount * 100, // convert to paise
+      currency: "INR",
+      receipt: `receipt_${Date.now()}`,
+    };
+
+    const order = await razorpay.orders.create(options)
+
+    await payment.create({
+      userId: req.userId,
+      planId,
+      amount,
+      credits,
+      razorpayOrderId: order.id,
+      status: "created",
+    });
+
+    return res.json(order);
+
+  } catch (error) {
+    return res.status(500).json({ message: `failed to get razorpay order ${error}` })
+  }
 }
 
 
-export const verifyPayment = async (req,res) => {
+export const verifyPayment = async (req, res) => {
   try {
-    const {razorpay_order_id,
+    const { razorpay_order_id,
       razorpay_payment_id,
-      razorpay_signature} = req.body
+      razorpay_signature } = req.body
 
     const body = razorpay_order_id + "|" + razorpay_payment_id;
 
@@ -51,7 +52,34 @@ export const verifyPayment = async (req,res) => {
       return res.status(400).json({ message: "Invalid payment signature" });
     }
 
-  } catch (error) {
+    const paymentDOC = await payment.findOne({
+      razorpayOrderId: razorpay_order_id,
+    });
 
+    if (!paymentDOC) {
+      return res.status(404).json({ message: "Payment not found" });
+    }
+
+    if (paymentDOC.status === "paid") {
+      return res.json({ message: "Already processed" });
+    }
+
+    // Update payment record
+    paymentDOC.status = "paid";
+    paymentDOC.razorpayPaymentId = razorpay_payment_id;
+    await paymentDOC.save();
+
+    // Add credits to user
+    const updatedUser = await User.findByIdAndUpdate(paymentDOC.userId, {
+      $inc: { credits: paymentDOC.credits }
+    }, { new: true });
+
+    res.json({
+      success: true,
+      message: "Payment verified and credits added",
+      user: updatedUser,
+    });
+  } catch (error) {
+     return res.status(500).json({ message: `failed to verify razorpay payment ${error}` })
   }
 }
